8bf8da1b58ea8632b58d3ac58191d78654c75415
Elias Lautaro Manca Wysocki <eliasmanca20@gmail.com>
2025-01-03 20:07:17 -0300

    ANDA CULIAAA ANDAAAAAAAA

--- functions.js
+++ functions.js
@@ -1,12 +1,3 @@
-$(document).ready(async function () {
-    await getCity();
-    await getWeatherFunction();
-    if (city != null) {
-        getWeather();
-        setInterval(getWeather, 1800000);
-    }
-})
-
 function updateClock() {
     const now = new Date();
     const hours = String(now.getHours()).padStart(2, '0');
@@ -24,6 +15,27 @@ updateClock();
 
 setInterval(updateClock, 1);
 
+window.wallpaperPropertyListener = {
+    applyUserProperties: function (properties) {
+
+        if (properties.weather) {
+            weatherFunction = properties.weather.value;
+        } else {
+            weatherFunction = true;
+        }
+
+        if (properties.city) {
+            city = properties.city.value;
+            getWeather();
+            setInterval(getWeather, 1800000);
+        } else {
+            city = '';
+        }
+
+        
+    }
+};
+
 const apiKey = "525ee573f0fffb372bd679c4ec659fa6";
 let url;
 let city;
@@ -34,8 +46,9 @@ setInterval(ensureSunAndMoon, 60000); // Cada segundo actualiza su posición
 
 async function getWeather() {
     try {
-        const response = await fetch(url);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${apiKey}&units=metric`);
         if (!response.ok) {
+
             throw new Error(`Error: ${response.status} - ${response.statusText}`);
         }
         const weatherData = await response.json();
@@ -45,7 +58,7 @@ async function getWeather() {
         $('#temperature').text(`${Math.round(weatherData.main.temp)}°C`);
         $('#weather').text(weatherData.weather[0].description);
 
-        stopAllAnimations(); 
+        stopAllAnimations();
 
         if (weatherFunction) {
             if (weatherDescription.includes('rain')) {
@@ -60,55 +73,8 @@ async function getWeather() {
         }
     } catch (error) {
         console.error("Hubo un problema con la solicitud del clima:", error);
-        $('#city').text("Not available");
+        $('#city').text("muere");
         $('#weather').text("Not available");
         stopAllAnimations();
-    }
-}
-
-async function getPropertyData() {
-    try {
-        // Carga el archivo JSON
-        const response = await fetch('project.json'); // Ruta al archivo
-        if (!response.ok) {
-            throw new Error(`Error al cargar el archivo: ${response.status}`);
-        }
-
-        const data = await response.json();
-        return data.general.properties;
-
-    } catch (error) {
-        console.error("Error al obtener los datos del archivo JSON:", error);
-    }
-}
-
-async function getCity() {
-    try {
-        const properties = await getPropertyData();
-        const data = properties.city?.value;
-
-        if (data && data !== '') {
-            city = data;
-            url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${apiKey}&units=metric`;
-        } else {
-            console.error("No se pudo obtener la ciudad del archivo JSON.");
-        }
-    } catch (error) {
-        console.error("Error al obtener la ciudad:", error);
-    }
-}
-
-async function getWeatherFunction() {
-    try {
-        const properties = await getPropertyData();
-        const data = properties.weather?.value;
-
-        if (data && data !== null) {
-            weatherFunction = data;
-        } else {
-            console.error("Weather function false.");
-        }
-    } catch (error) {
-        console.error("Error al obtener la funcionalidad del clima:", error);
     }
 }
